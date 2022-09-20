b0a5afe73489120bcecc82311f843fe1d52f9fa7
Charisis Samaras <xsxsam2015@gmail.com>
2022-09-20 15:22:16 +0300

    Add a bit of validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,44 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    } else setEnteredNameIsValid(true);
+
+    console.log(enteredName);
+    console.log(nameInputRef.current.value);
+    setEnteredName("");
+  };
+
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
+      {!enteredNameIsValid && <p className="error-text">Name is not valid</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
